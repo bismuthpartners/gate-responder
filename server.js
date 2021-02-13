6feaf238d07db0f3b6085c55e03417971d1eadd4
Daniel Zhou <danielzhou@Daniels-MacBook-Pro.local>
2021-02-13 12:16:29 -0600

    customcode

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ http
     // Create TwiML response
     const twiml = new VoiceResponse();
 
-    twiml.say('Hello from your pals at Twilio! Have fun.');
+    twiml.say('Code works, its authentic.');
 
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
